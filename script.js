a1ee8559f58f75b77e87f33d034f36059327109a
Bandar-Zuhair <146974861+Bandar-Zuhair@users.noreply.github.com>
2024-11-23 18:39:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ let webAppURL = 'https://script.google.com/macros/s/AKfycbzUrB4LgdcWid4u3LqjZm-f
 
 
 
-/* Insert new click data in the Google Sheet */
 function insertNewClick(columnName) {
-  const scriptURL = "https://script.google.com/macros/s/AKfycbz6E6IKXBZUhyMEnJSi7_osjGE54VztLgRHMtNqOrUnN6ZsQ5w5KxdUjTPn2ZXHSAnv/exec";
+  const scriptURL = "https://script.google.com/macros/s/AKfycbyU-p7z3tHF0I1K0GCmjcRG3CaG0NPkGyMPTvhlGPISxwIYrt6ueD7O2iHSza9SPOP3/exec";
 
-  fetch(`${scriptURL}?columnName=${encodeURIComponent(columnName)}`)
+  // Trim the column name before passing it
+  fetch(`${scriptURL}?columnName=${encodeURIComponent(columnName.trim())}`)
     .then(response => response.text())
     .then(data => console.log("Response:", data))
     .catch(error => console.error("Error:", error));
 }
+
+
 
 
 
@@ -77,14 +79,14 @@ async function fetchAndDisplayProducts() {
         // Add a new showcase product HTML
         let productHtml = `
           <div class="showcase">
-            <a onclick="insertNewClick(" affiliate marketing 1 ")" href="${productAffiliateLink}" target="_blank" class="showcase-img-box">
+            <a onclick="insertNewClick('affiliate marketing 1')" href="${productAffiliateLink}" target="_blank" class="showcase-img-box">
               <img src="${productImage}" width="70" class="showcase-img">
             </a>
             <div class="showcase-content">
-              <a onclick="insertNewClick(" affiliate marketing 1 ")" href="${productAffiliateLink}" target="_blank">
+              <a onclick="insertNewClick('affiliate marketing 1')" href="${productAffiliateLink}" target="_blank">
                 <h4 class="showcase-title">${productName}</h4>
               </a>
-              <a onclick="insertNewClick(" affiliate marketing 1 ")" href="${productAffiliateLink}" target="_blank" class="showcase-category">${productTypeName}</a>
+              <a onclick="insertNewClick('affiliate marketing 1')" href="${productAffiliateLink}" target="_blank" class="showcase-category">${productTypeName}</a>
               <div class="price-box">
                 <p class="price">${productCurrentCost}</p>
                 <del class="old-price">${productOldCost}</del>
